12af79cfaa55005e485735922cfb25b1c719dd5b
YeAns <3124853730@qq.com>
2026-06-09 14:00:44 +0800

    Update __init__.py

--- threestudio/models/materials/__init__.py
+++ threestudio/models/materials/__init__.py
@@ -4,6 +4,9 @@ from . import (
     hybrid_rgb_latent_material,
     neural_radiance_material,
     no_material,
-    pbr_material,
-    sd_latent_adapter_material,
 )
+
+try:
+    from . import pbr_material
+except Exception:
+    print("Skip pbr_material")
